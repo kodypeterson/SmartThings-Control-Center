c2349b61d40c9608730d73cd6e4a12906e428286
Kody Peterson <kodypeterson91@gmail.com>
2015-12-21 14:53:15 -0500

    An attempt at HTTP pass-through

--- actions/ftpProxy.js
+++ actions/ftpProxy.js
@@ -8,6 +8,9 @@ exports.ftpProxy = {
         },
         size: {
             required: true
+        },
+        http: {
+            require: false
         }
     },
 
@@ -16,33 +19,44 @@ exports.ftpProxy = {
     },
 
     run: function(api, data, next){
-        var server = api.servers.servers[data.connection.type];
-        var JSFtp = require("jsftp");
         var config = require('../tasks/config.json');
+        var server = api.servers.servers[data.connection.type];
 
-        var ftp = new JSFtp({
-            host: config.domain.replace('http://', ''),
-            user: config.ftpUser, // defaults to "anonymous"
-            pass: config.ftpPass // defaults to "@anonymous"
-        });
+        if (!data.params.http) {
+            var JSFtp = require("jsftp");
 
-        ftp.get(data.params.path, function(err, socket) {
-            if (err) return;
-
-            socket.on("close", function(hadErr) {
-                if (hadErr)
-                    console.error('There was an error retrieving the file.');
-
-                data.connection.rawConnection.res.end();
-                data.connection.destroy();
+            var ftp = new JSFtp({
+                host: config.domain.replace('http://', ''),
+                user: config.ftpUser, // defaults to "anonymous"
+                pass: config.ftpPass // defaults to "@anonymous"
             });
 
+            ftp.get(data.params.path, function (err, socket) {
+                if (err) return;
+
+                socket.on("close", function (hadErr) {
+                    if (hadErr)
+                        console.error('There was an error retrieving the file.');
+
+                    data.connection.rawConnection.res.end();
+                    data.connection.destroy();
+                });
+
+                var type = data.params.path.split('.');
+                type = type[type.length - 1];
+
+                server.sendFile(data.connection, null, socket, 'image/' + type, data.params.size);
+
+                socket.resume();
+            });
+        } else {
+            var url = config.domain + '/api/2.7/rest/file_contents' + data.params.path + '?device_user_id=19408415&device_user_auth_code=' + config.authCode;
             var type = data.params.path.split('.');
-            type = type[type.length-1];
+            type = type[type.length - 1];
+            var request = require('request');
+            var requestStream = request(url);
 
-            server.sendFile(data.connection, null, socket,  'image/' + type, data.params.size);
-
-            socket.resume();
-        });
+            server.sendFile(data.connection, null, requestStream, 'image/' + type, data.params.size);
+        }
     }
 };
