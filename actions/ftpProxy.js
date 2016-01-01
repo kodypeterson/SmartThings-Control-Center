5def1052e058b1315709d432fcd98678aa83b1ae
Kody Peterson <kodypeterson91@gmail.com>
2016-01-01 16:31:40 -0500

    - Properly rotate image based on EXIF data

--- actions/ftpProxy.js
+++ actions/ftpProxy.js
@@ -47,6 +47,9 @@ exports.ftpProxy = {
         var request = require('request');
         var fs = require('fs');
         var imgLocation = __dirname + '/../public/tempImage.' + type;
+
+
+
         var ws = fs.createWriteStream(imgLocation);
 
         request({
@@ -57,11 +60,64 @@ exports.ftpProxy = {
         ws.on('finish', function(err) {
             data.toRender = false;
 
-            var jpeg = require("jpegorientation");
-            jpeg.autoRotate(imgLocation, imgLocation, function(err) {
-                data.connection.rawConnection.responseHttpCode = 200;
-                data.connection.sendFile('tempImage.' + type);
-                next(data, false);
+            //var jpeg = require("jpegorientation");
+            //jpeg.autoRotate(imgLocation, imgLocation, function(err) {
+            //    data.connection.rawConnection.responseHttpCode = 200;
+            //    data.connection.sendFile('tempImage.' + type);
+            //    next(data, false);
+            //});
+
+            var fs = require('fs');
+            var exif = require('exif-parser');
+            var lwip = require('lwip');
+
+            // path is the path to your image
+            fs.readFile(imgLocation, function (err, d) {
+                if (err) throw err;
+                var exifData = false;
+                // ext is the extension of the image
+                if(type.toLowerCase() == "jpg"){
+                    exifData = exif.create(d).parse();
+                }
+                lwip.open(d, type.toLowerCase(), function(err, image){
+                    if(err) throw err;
+                    if(exifData){
+                        switch( exifData.tags.Orientation ) {
+                            case 2:
+                                image = image.batch().flip('x'); // top-right - flip horizontal
+                                break;
+                            case 3:
+                                image = image.batch().rotate(180); // bottom-right - rotate 180
+                                break;
+                            case 4:
+                                image = image.batch().flip('y'); // bottom-left - flip vertically
+                                break;
+                            case 5:
+                                image = image.batch().rotate(90).flip('x'); // left-top - rotate 90 and flip horizontal
+                                break;
+                            case 6:
+                                image = image.batch().rotate(90); // right-top - rotate 90
+                                break;
+                            case 7:
+                                image = image.batch().rotate(270).flip('x'); // right-bottom - rotate 270 and flip horizontal
+                                break;
+                            case 8:
+                                image = image.batch().rotate(270); // left-bottom - rotate 270
+                                break;
+                        }
+                    }else{
+                        image = image.batch();
+                    }
+
+                    image.writeFile(imgLocation, function(err, buffer) {
+                        //server.sendFile(data.connection, null, buffer, 'image/jpg', data.params.size, new Date().getTime());
+                        data.connection.rawConnection.responseHttpCode = 200;
+                        data.connection.sendFile('tempImage.' + type);
+                        next(data, false);
+                    });
+                    // image can now be used as per normal with batch
+                    // eg. image.resize(200, 200)....
+                });
             });
         });
     }
